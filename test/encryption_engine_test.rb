79c35dba035601ce9cb5af11c56e1eaf321aadc5
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-12 09:37:39 -0600

    Add generate keys test to EncryptionEngine test, change arguments to strings, comment out encryption test on Enigma

--- test/encryption_engine_test.rb
+++ test/encryption_engine_test.rb
@@ -3,10 +3,14 @@ require 'minitest/pride'
 require './lib/encryption_engine'
 class EncryptionEngineTest < Minitest::Test
   def setup
-    @encryption_engine = EncryptionEngine.new("hello world", 02715, 040895)
+    @encryption_engine = EncryptionEngine.new("hello world", "02715",  "040895")
   end
 
   def test_it_exists
     assert_instance_of EncryptionEngine, @encryption_engine
+  end
+
+  def test_it_can_generate_keys
+    assert_equal ["12", "23", "34", "45"], @encryption_engine.generate_keys("12345")
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,9 +14,9 @@ class EnigmaTest < Minitest::Test
     assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], @enigma.alphabet
   end
 
-  def test_it_can_encrypt_a_message
-    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
-
-    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-  end
+  # def test_it_can_encrypt_a_message
+  #   expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+  #
+  #   assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  # end
 end
