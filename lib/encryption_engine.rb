5364bf8c862d9dc2f398f71a23f35ade51b1ed93
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-12 10:32:27 -0600

    Add initial offset method to EncryptionEngine

--- lib/encryption_engine.rb
+++ lib/encryption_engine.rb
@@ -14,7 +14,14 @@ class EncryptionEngine
   # end
 
   def offset(date, key_array)
-
+    offsets = date.to_i.abs2.to_s
+    offset_index = -4
+    key_array.map do |key|
+      key_offset = offsets[offset_index].to_i
+      key_int = key.to_i
+      offset_index += 1
+      (key_int += key_offset).to_s
+    end
   end
 
   def generate_keys(key)
