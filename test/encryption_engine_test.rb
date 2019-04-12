813be6cc5954f59bd96bee7d36fcebc6b5f9c152
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-12 09:15:02 -0600

    Add initial Encryption Engine test

---
+++ test/encryption_engine_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encryption_engine'
+class EncryptionEngineTest < Minitest::Test
+  def setup
+    @encryption_engine = EncryptionEngine.new("hello world", 02715, 040895)
+  end
+
+  def test_it_exists
+    assert_instance_of EncryptionEngine, @encryption_engine
+  end
+end
