c100d77f4d8a95f4d7de20ea7decd9f4b7682a45
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-12 09:19:20 -0600

    Add Enigma message encrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,4 +13,10 @@ class EnigmaTest < Minitest::Test
   def test_it_has_an_alphabet
     assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], @enigma.alphabet
   end
+
+  def test_it_can_encrypt_a_message
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
 end
