6c1d27e2928da80dd940c4efc19d47898e0ad5fd
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-16 12:18:04 -0600

    Add comma in the middle #decrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -68,4 +68,10 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, @enigma.encrypt("hello, world", "02715", "040895")
   end
+
+  def test_it_can_decrypt_a_message_with_a_comma
+    expected = {decryption: "hello, world", key: "02715", date: "040895"}
+
+    assert_equal expected, @enigma.decrypt("keder,sprrdx", "02715", "040895")
+  end
 end
