7d06de105937658c8af88a68984f381e714a1628
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-16 13:14:30 -0600

    Fix file_path for File.read, Fix typo on encrypted local variable

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,8 +1,10 @@
 require './lib/enigma'
 message_file, encrypted_file = ARGV
 
-message = File.read(message_file)
+message = File.read("./lib/#{message_file}")
 
 enigma = Enigma.new
 
-enrypted = enigma.encrypt(message)
+encrypted = enigma.encrypt(message)
+
+require "pry"; binding.pry
